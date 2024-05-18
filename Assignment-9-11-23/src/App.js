65916b6ce4e0f545a9384777958f0dee91d6683d
Jesaiah Richard Raja E <109457982+JesaiahRichard@users.noreply.github.com>
2024-05-18 11:02:00 +0530

    Add files via upload

---
+++ Assignment-9-11-23/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Card1 from './Card1';
+import './Card.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section class="pricing py-5">
+          <div class="container">
+            <div class="row">
+              <Card1/>
+               
+            </div>
+          </div>
+      </section>
+ 
+  
+    </div>
+  );
+}
+
+export default App;
